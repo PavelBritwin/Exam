4451d7b79d5fb39cd21bcdb6786577571d4f1f17
Pavel Anokhin <anohinpa@gmail.com>
2022-12-04 20:23:23 +0300

    Create string array

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// Create array
+Console.WriteLine("Введите количество элементов строкового массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+string [] strArr = new string[n];
+for (int i = 0; i < n; i++)
+{
+    Console.WriteLine("Введите элемент "+i+": ");
+    strArr[i] = Console.ReadLine();
+}
